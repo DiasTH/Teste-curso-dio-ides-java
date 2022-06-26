a85967046747e108b8a8a554c96ef4f96316d638
DiasTH <tharleydias.adv@gmail.com>
2022-06-26 00:54:19 -0300

    teste java

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+	}
+
+}
+
